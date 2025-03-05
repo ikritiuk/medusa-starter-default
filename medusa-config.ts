9662fb09d45b70b1504e42c44a56e87a13b74a7e
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2025-03-05 13:32:31 +0100

    sendgrid was removed

--- medusa-config.ts
+++ medusa-config.ts
@@ -60,23 +60,6 @@ module.exports = defineConfig({
                 },
             },
         },
-        {
-            resolve: "@medusajs/medusa/notification",
-            options: {
-                providers: [
-                    // ...
-                    {
-                        resolve: "@medusajs/medusa/notification-sendgrid",
-                        id: "sendgrid",
-                        options: {
-                            channels: ["email"],
-                            api_key: process.env.SENDGRID_API_KEY,
-                            from: process.env.SENDGRID_FROM,
-                        },
-                    },
-                ],
-            },
-        },
 
     ],
 
