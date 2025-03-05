317422279d5e620abe0275f82b8dfb07b2ddb87c
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2025-03-05 17:12:59 +0100

    custom mail service was created v2 with npm install nodemailer revert

--- src/services/index.ts
+++
@@ -1,7 +0,0 @@
-import EmailService from "./email-service";
-import OrderSubscriber from "../subscribers/order-subscriber";
-
-export default (container) => {
-    container.register("emailService", () => new EmailService());
-    container.register("orderSubscriber", (c) => new OrderSubscriber(c.resolve("eventBusService"), c.resolve("emailService")));
-};
