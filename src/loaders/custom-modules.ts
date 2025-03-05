6a36baed57f51543ed96f7d3cbce79b8c36a67e5
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2025-03-05 16:43:10 +0100

    custom mail service was created v2 was removed

--- src/loaders/custom-modules.ts
+++
@@ -1,6 +0,0 @@
-import { asClass } from "awilix";
-import CustomEmailService from "../modules/custom-email";
-
-export default async ({ container }) => {
-    container.register("customEmailService", asClass(CustomEmailService));
-};
--- src/loaders/index.ts
+++
@@ -1,5 +0,0 @@
-import customModulesLoader from "./custom-modules";
-
-export default async (container) => {
-    await customModulesLoader({ container });
-};
