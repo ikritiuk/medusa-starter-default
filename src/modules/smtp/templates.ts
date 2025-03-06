f3702c00c3124de27f5fb16a7c636d17bd24bda7
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2025-03-06 11:00:03 +0100

    fix morning

--- src/modules/smtp/templates.ts
+++ src/modules/smtp/templates.ts
@@ -1,8 +1,7 @@
-export type EmailTemplateType = keyof typeof EmailTemplates
 export const EmailTemplates = {
     ORDER_PLACED: "order-placed",
     ORDER_SHIPPED: "order-shipped",
     ORDER_DELIVERED: "order-delivered",
 } as const
 
-export type EmailTemplateType = (typeof EmailTemplates)[keyof typeof EmailTemplates]
+export type EmailTemplateType = keyof typeof EmailTemplates
