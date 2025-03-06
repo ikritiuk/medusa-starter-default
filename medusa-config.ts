b9a6e075fb758f1504cd195c8e88e9221ffae972
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2025-03-06 10:41:40 +0100

    fix morning

--- medusa-config.ts
+++ medusa-config.ts
@@ -31,7 +31,7 @@ module.exports = defineConfig({
                 providers: [
                     {
                         resolve: "./src/modules/smtp",
-                        id: "smtp",
+                        id: "notification-smtp",
                         options: {
                             channels: ["email"],
                             host: process.env.SMTP_HOST,
