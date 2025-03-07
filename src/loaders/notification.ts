a659779e98eb6f2311660bfc34df86b720f4b6be
ilia.kritiuk <ilia.kritiuk@diva-e.com>
2025-03-07 13:18:38 +0100

    notification.ts WAS REMOVED

--- src/loaders/notification.ts
+++
@@ -1,9 +0,0 @@
-import { MedusaContainer, Modules } from "@medusajs/framework/utils"
-import { INotificationModuleService } from "@medusajs/framework/types"
-
-export default async (container: MedusaContainer): Promise<void> => {
-    const notificationModuleService: INotificationModuleService =
-        container.resolve(Modules.NOTIFICATION) // Correct way to resolve notification service in Medusa v2
-
-    notificationModuleService.subscribe("order.placed", "email") // Ensure "email" matches your provider identifier
-}
